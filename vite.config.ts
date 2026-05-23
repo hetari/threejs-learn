e3ce25d5e16c67b7b382f97669ea00f82ee1bd4f
hetari <hetari4py@gmail.com>
2026-05-23 20:19:31 +0300

    feat: configure base path for production deployment and update router history to support subdirectories

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import VueRouter from 'vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/threejs-learn/",
   plugins: [
     VueRouter(),
     vue(templateCompilerOptions), tailwindcss()],
