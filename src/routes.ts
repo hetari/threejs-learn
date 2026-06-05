480ee0a0c50aff44ad12ae7397520c96e64198d9
hetari <hetari4py@gmail.com>
2026-06-05 23:06:01 +0300

    refactor: switch vue-router to hash history mode

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes, 
 })
 
